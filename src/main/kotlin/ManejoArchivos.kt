a163df92070cc53f9ee5efc4622a5c8ca6bcfb86
belen001 <b.bravo02@ufromail.cl>
2023-07-03 21:07:27 -0400

    metodos ManejoArchivos

--- src/main/kotlin/ManejoArchivos.kt
+++ src/main/kotlin/ManejoArchivos.kt
@@ -1,2 +1,28 @@
-class ManejoArchivos {
+import java.io.File
+class ManejoArchivos(private val nombreArchivo: String) {
+    fun crearArchivo() {
+        val archivo = File(nombreArchivo)
+        archivo.createNewFile()
+    }
+
+    fun escribirEnArchivo(contenido: String) {
+        val archivo = File(nombreArchivo)
+        archivo.appendText("$contenido\n")
+    }
+
+    fun leerArchivo(): List<String> {
+        val archivo = File(nombreArchivo)
+        return archivo.readLines()
+    }
+
+    fun VaciarArchivo() {
+        val archivo = File(nombreArchivo)
+        archivo.writeText("")
+    }
+
+    fun eliminarArchivo() {
+        val archivo = File(nombreArchivo)
+        archivo.delete()
+    }
+
 }
