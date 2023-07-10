d161e6373e8161379bbc1e136d9e8dc2b1117ade
belen001 <b.bravo02@ufromail.cl>
2023-07-10 19:35:56 -0400

    corrección enum

--- src/main/kotlin/UnoCartas.kt
+++ src/main/kotlin/UnoCartas.kt
@@ -3,11 +3,11 @@ enum class UnoCartas (val valor: Int, val color: String){
     ROJOCERO(0, "Rojo"), ROJOUNO(1, "Rojo"), ROJODOS(2, "Rojo"), ROJOTRES(3, "Rojo"), ROJOCUATRO(4, "Rojo"), ROJOCINCO(5, "Rojo"),
     ROJOSEIS(6, "Rojo"), ROJOSIETE(7, "Rojo"), ROJOOCHO(8, "Rojo"), ROJONUEVE(9, "Rojo"),
     // Cartas ROJAS especiales
-    REVERSA(20, "Rojo"), SALTO(20, "Rojo"), DOSMAS(20, "Rojo"),
+    ROJOREVERSA(20, "Rojo"), ROJOSALTO(20, "Rojo"), ROJODOSMAS(20, "Rojo"),
 
     // Cartas Azules
-    AZULCERO(0, "Azul"), AZULUNO(1, "Azul"), AZULDOS(2, "Azul"), AZULTRES(3, "Azul"), AZULCUATRO(4, "AZUL"), AZULCINCO(5, "Azul"),
-    AZULSEIS(6, "Azul"), AZULSIETE(7, "AZUL"), AZULOCHO(8, "Azul"), AZULNUEVE(9, "Azul"),
+    AZULCERO(0, "Azul"), AZULUNO(1, "Azul"), AZULDOS(2, "Azul"), AZULTRES(3, "Azul"), AZULCUATRO(4, "Azul"), AZULCINCO(5, "Azul"),
+    AZULSEIS(6, "Azul"), AZULSIETE(7, "Azul"), AZULOCHO(8, "Azul"), AZULNUEVE(9, "Azul"),
     // Cartas Azules especiales
     AZULREVERSA(20, "Azul"), AZULSALTO(20, "Azul"), AZULDOSMAS(20, "Azul"),
 
@@ -16,8 +16,9 @@ enum class UnoCartas (val valor: Int, val color: String){
     AMARILLOSEIS(6, "Amarillo"), AMARILLOSIETE(7, "Amarillo"), AMARILLOOCHO(8, "Amarillo"), AMARILLONUEVE(9, "Amarillo"),
     // Cartas Amarillas especiales
     AMARILLOREVERSA(20, "Amarillo"), AMARILLOSALTO(20, "Amarillo"), AMARILLODOSMAS(20, "Amarillo"),
+
     // Cartas Verdes
-    VERDECERO(0, "Verde"), VERDEUNO(1, "Verde"), VERDEDOS(2, "Verde"), VERDETRES(3, "Verde"), VERDECUATRO(4, "VERDE"), VERDECINCO(5, "Verde"),
+    VERDECERO(0, "Verde"), VERDEUNO(1, "Verde"), VERDEDOS(2, "Verde"), VERDETRES(3, "Verde"), VERDECUATRO(4, "Verde"), VERDECINCO(5, "Verde"),
     VERDESEIS(6, "Verde"), VERDESIETE(7, "Verde"), VERDEOCHO(8, "Verde"), VERDENUEVE(9, "Verde"),
     // Cartas Verdes especiales
     VERDEREVERSA(20, "Verde"), VERDESALTO(20, "Verde"), VERDEDOSMAS(20, "Verde"),
